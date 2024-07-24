093c98c856322bd785983131f53458034a553568
VKTRenokh <viktorenokh@gmail.com>
2024-07-24 13:37:01 +0300

    feat: add tap dance for h and l

--- keyboards/splitkb/aurora/corne/keymaps/VKTRenokh/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/VKTRenokh/keymap.c
@@ -3,6 +3,17 @@
 
 #define DONT_LIGHT_UP_PIN ;
 
+
+enum custom_keycodes {
+    TD_LEFT_HOME,
+    TD_RIGHT_END
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LEFT_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT, KC_HOME),
+    [TD_RIGHT_END] = ACTION_TAP_DANCE_DOUBLE(KC_RIGHT, KC_END),
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -10,14 +21,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, QK_GESC,
                                     KC_LGUI,  MO(1),   KC_SPC,  KC_ENT,  MO(2),  KC_RALT
     ),
 
     // Layer 1: Number layer
     [1] = LAYOUT_split_3x6_3(
         KC_TAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_LCTL,  KC_ESC,  CW_TOGG, KC_CAPS, XXXXXXX, QK_REP,        KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT, XXXXXXX, KC_CAPS,
+        KC_LCTL,  KC_ESC,  CW_TOGG, KC_CAPS, XXXXXXX, QK_REP,        TD(TD_LEFT_HOME), KC_DOWN,  KC_UP,   TD(TD_RIGHT_END), XXXXXXX, KC_CAPS,
         KC_LSFT,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CW_TOGG,
                                     KC_LGUI,  _______, KC_SPC,  KC_ENT,  MO(3),  KC_LGUI
     ),
